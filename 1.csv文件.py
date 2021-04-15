235e4fc99b64db1270dbc7e7222634a2019f4338
vegtabeldog <1102228498@qq.com>
2021-04-15 15:27:44 +0800

    master

--- 1.csv文件.py
+++ 1.csv文件.py
@@ -16,3 +16,9 @@ print(pd.read_csv(csv_file, index_col=0))
 diabetes = pd.read_csv("data/home/aistudio/data/data20465/diabetes.csv")
 print(diabetes)
 print(diabetes.head(3))
+
+
+bike = pd.read_csv("data/home/aistudio/data/data20465/Bicycle_Counts.csv",index_col=0)
+print(bike.head(10))
+print(bike.shape)
+print(bike.shape[0])
